5f07fb8338f90e9dcdf8ef4d7454bcfd03a1f4d1
nguyencaoantuong <ntuong@proton.me>
2025-12-02 20:00:08 +0700

    fix: argument type mismatch

--- app/src/main/java/com/heliumOrganization/heliumLabs/ui/theme/Theme.kt
+++ app/src/main/java/com/heliumOrganization/heliumLabs/ui/theme/Theme.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
+import com.heliumOrganization.heliumLabs.ui.theme.Typography
 import com.eclipseLaboratory.eclipseLabs.ui.theme.Pink40
 import com.eclipseLaboratory.eclipseLabs.ui.theme.Pink80
 import com.eclipseLaboratory.eclipseLabs.ui.theme.Purple40
